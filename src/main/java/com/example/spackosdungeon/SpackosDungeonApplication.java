79a3bd7c2e10d97d7d44a5ce9e877c90a953a40b
MagnoTheFirst <alejandro.r.laneri@gmail.com>
2024-02-24 19:27:18 +0100

    first lady

---
+++ src/main/java/com/example/spackosdungeon/SpackosDungeonApplication.java
@@ -0,0 +1,13 @@
+package com.example.spackosdungeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpackosDungeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpackosDungeonApplication.class, args);
+    }
+
+}
